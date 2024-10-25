8a7b74afffcc8858bea7075342028f05e9bfab55
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-10-25 21:39:22 +0200

    Update main application configuration in app.js

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ import bodyParser from 'body-parser';
 import userRouter from './src/routes/User.js';
 import expenseCategoryRouter from './src/routes/ExpenseCategory.js'; 
 import paymentMethodRouter from './src/routes/PaymentMethod.js'; 
+import expenseRouter from './src/routes/Expense.js';
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/api', userRouter);
 app.use('/api', expenseCategoryRouter);
 app.use('/api', paymentMethodRouter); 
+app.use('/api', expenseRouter);
 // Route de test pour vérifier que l'application fonctionne
 app.get('/', (req, res) => {
-  res.send("Bienvenue dans l'API des utilisateurs, des catégories de dépenses et des méthodes de paiement");
+  res.send("Bienvenue dans mon application");
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
