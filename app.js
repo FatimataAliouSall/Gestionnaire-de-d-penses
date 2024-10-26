5d0a3a6a804465ebef7aa76298d7ee9aeee21c4c
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-10-26 20:12:30 +0200

    Update application configuration in app.js

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ import userRouter from './src/routes/User.js';
 import expenseCategoryRouter from './src/routes/ExpenseCategory.js'; 
 import paymentMethodRouter from './src/routes/PaymentMethod.js'; 
 import expenseRouter from './src/routes/Expense.js';
-import planningRouter from './src/routes/Planning.js'; 
+import paymentRouter from './src/routes/Payment.js'; 
+import planningRouter from './src/routes/Planning.js';
 
 const app = express();
 app.use(bodyParser.json());
-
 app.use('/api', userRouter);
 app.use('/api', expenseCategoryRouter);
-app.use('/api', paymentMethodRouter); 
+app.use('/api', paymentMethodRouter);
 app.use('/api', expenseRouter);
-app.use('/api', planningRouter); 
-
+app.use('/api', paymentRouter); 
+app.use('/api', planningRouter);
 app.get('/', (req, res) => {
   res.send("Bienvenue dans mon application");
 });
