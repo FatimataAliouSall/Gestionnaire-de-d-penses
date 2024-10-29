a1aaa0510101b7886c3125f310f6d982b33470e6
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-10-29 14:00:01 +0100

    Update application configuration in app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import i18next from 'i18next';
+import i18nextMiddleware from 'i18next-http-middleware';
+import Backend from 'i18next-fs-backend';
 
 import userRouter from './src/routes/User.js';
 import expenseCategoryRouter from './src/routes/ExpenseCategory.js';
@@ -7,8 +10,28 @@ import paymentMethodRouter from './src/routes/PaymentMethod.js';
 import expenseRouter from './src/routes/Expense.js';
 import paymentRouter from './src/routes/Payment.js';
 import planningRouter from './src/routes/Planning.js';
+import loginRoute from './src/api/login.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+
+i18next
+  .use(Backend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    fallbackLng: 'fr',
+    backend: {
+      loadPath: './locales/{{lng}}/translation.json',
+    },
+    detection: {
+      order: ['querystring', 'cookie', 'header'],
+      caches: ['cookie'],
+    },
+  });
+
+app.use(i18nextMiddleware.handle(i18next));
+
 app.use(bodyParser.json());
 app.use('/api', userRouter);
 app.use('/api', expenseCategoryRouter);
@@ -16,8 +39,10 @@ app.use('/api', paymentMethodRouter);
 app.use('/api', expenseRouter);
 app.use('/api', paymentRouter);
 app.use('/api', planningRouter);
+app.use('/api', loginRoute);
+
 app.get('/', (req, res) => {
-  res.send('Bienvenue dans mon application');
+  res.send(req.t('Bienvenue dans mon application'));
 });
 
 const PORT = process.env.PORT || 3000;
