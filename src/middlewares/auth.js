bb1e5f01cc8207d387e4d7a8dad9752d88d535c7
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-11-28 00:03:01 +0100

    Update authentication middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,4 @@
-// authMiddleware.js
+
 import jwt from 'jsonwebtoken';
 
 export const authMiddleware = (req, res, next) => {
@@ -18,3 +18,34 @@ export const authMiddleware = (req, res, next) => {
     res.status(400).json({ message: 'Invalid token.' });
   }
 };
+
+// import jwt from 'jsonwebtoken';
+// export const authMiddleware = (req, res, next) => {
+//   const token = null;
+//   req.header('Authorization')?.split(' ')[1];
+
+//   if (!token) {
+//     return res
+//       .status(401)
+//       .json({ message: 'Access denied. No token provided.' });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded; 
+//     next();
+//   } catch (error) {
+//     res.status(401).json({ message: 'Invalid token.' });
+//   }
+// };
+
+// export const authorizeRoles = (...roles) => {
+//   return (req, res, next) => {
+//     if (!req.user || !roles.includes(req.user.role)) {
+//       return res
+//         .status(403)
+//         .json({ message: 'Access denied. You are not authorized.' });
+//     }
+//     next();
+//   };
+// };
