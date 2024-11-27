6a9933d8abf1866dcc7f37ab5483b45d1b868b95
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-11-28 00:04:44 +0100

    Update expense validation

--- src/validators/ExpenseValidator.js
+++ src/validators/ExpenseValidator.js
@@ -21,26 +21,9 @@ const createExpenseValidator = [
     .bail()
     .isLength({ min: 3, max: 50 })
     .withMessage('Le titre doit comporter entre 3 et 50 caractères.'),
-  check('amount')
-    .notEmpty()
-    .withMessage('Le montant est obligatoire.')
-    .bail()
-    .isDecimal({ decimal_digits: '0,2' })
-    .withMessage(
-      'Le montant doit être un nombre positif avec deux chiffres après la virgule maximum.'
-    ),
-  check('frequency')
-    .optional()
-    .isIn(['Mensuel', 'Annuel', 'Hebdomadaire'])
-    .withMessage('La fréquence doit être \'Mensuel', 'Annuel', 'Hebdomadaire.'),
-  check('startDate')
-    .optional()
-    .isISO8601()
-    .withMessage('La date de début doit être une date valide (format ISO).'),
-  check('endDate')
-    .optional()
-    .isISO8601()
-    .withMessage('La date de fin doit être une date valide (format ISO).'),
+  
+  
+ 
   // check('userId')
   //   .optional()
   //   .isInt()
@@ -90,22 +73,7 @@ const updateExpenseValidator = [
     .optional()
     .isLength({ min: 3, max: 50 })
     .withMessage('Le titre doit comporter entre 3 et 50 caractères.'),
-  check('amount')
-    .optional()
-    .isFloat({ min: 0 })
-    .withMessage('Le montant doit être un nombre positif.'),
-  check('frequency')
-    .optional()
-    .isIn(['mensuel', 'annuel', 'hebdomadaire'])
-    .withMessage('La fréquence doit être \'Mensuel', 'Annuel', 'Hebdomadaire.'),
-  check('startDate')
-    .optional()
-    .isISO8601()
-    .withMessage('La date de début doit être une date valide (format ISO).'),
-  check('endDate')
-    .optional()
-    .isISO8601()
-    .withMessage('La date de fin doit être une date valide (format ISO).'),
+  
   // check('userId')
   //   .optional()
   //   .isInt()
