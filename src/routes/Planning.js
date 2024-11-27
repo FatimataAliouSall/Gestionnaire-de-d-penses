2a32a8578557ec31864e5228e57d7d92274761b3
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-11-27 23:55:58 +0100

    Update routes for planning

--- src/routes/Planning.js
+++ src/routes/Planning.js
@@ -5,37 +5,42 @@ import {
   updatePlanningValidator,
   deletePlanningValidator,
 } from '../validators/PlanningValidator.js';
-// import { authMiddleware } from '../middlewares/auth.js';
+import { authMiddleware } from '../middlewares/auth.js';
+// import { authRoleAdmin } from '../middlewares/authRoleAdmin.js';
 const router = express.Router();
 
 router.post(
   '/planning',
-  // authMiddleware,
+  authMiddleware,
+  // authRoleAdmin,
   createPlanningValidator,
   PlanningController.createPlanning
 );
 
-router.get('/planning',
-  //  authMiddleware, 
-   PlanningController.getAllPlannings);
+router.get('/planning', authMiddleware, 
+  // authRoleAdmin, 
+  PlanningController.getAllPlannings);
 
 router.get(
   '/planning/:id',
-  // authMiddleware,
+  authMiddleware,
+  // authRoleAdmin,
   deletePlanningValidator,
   PlanningController.getPlanningById
 );
 
 router.put(
   '/planning/:id',
-  // authMiddleware,
+  authMiddleware,
+  // authRoleAdmin,
   updatePlanningValidator,
   PlanningController.updatePlanning
 );
 
 router.delete(
   '/planning/:id',
-  // authMiddleware,
+  authMiddleware,
+  // authRoleAdmin,
   deletePlanningValidator,
   PlanningController.deletePlanning
 );
