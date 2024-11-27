cec1a3c467b2a3950fec28597fdc7276151f25d9
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-11-28 00:04:24 +0100

    Update user routes

--- src/routes/User.js
+++ src/routes/User.js
@@ -5,38 +5,35 @@ import {
   updateUserValidator,
   deleteUserValidator,
 } from '../validators/UserValidator.js';
-// import { authMiddleware } from '../middlewares/auth.js';
+import { authMiddleware } from '../middlewares/auth.js';
 const router = express.Router();
 
 router.post(
   '/users',
-  // authMiddleware,
+  authMiddleware,
   createUserValidator,
   UserController.createUser
 );
-router.post(
-  '/login',
-  // authMiddleware,
-  createUserValidator,
-  UserController.login
-);
-router.get('/users/:id', 
-  // authMiddleware,
-   UserController.getUser);
-router.get('/users', 
-  // authMiddleware,
-   UserController.getAllUsers);
+
+router.get('/users/:id', authMiddleware, UserController.getUser);
+router.get('/profile', authMiddleware, UserController.getProfile);
+router.get('/users', authMiddleware, UserController.getAllUsers);
 router.put(
   '/users/:id',
-  // authMiddleware,
+  authMiddleware,
+
   updateUserValidator,
   UserController.updateUser
 );
 router.delete(
   '/users/:id',
-  // authMiddleware,
+  authMiddleware,
   deleteUserValidator,
   UserController.deleteUser
 );
+
+router.post('/request-password-reset', UserController.requestPasswordReset);
+router.post('/reset-password', UserController.resetPassword);
+router.put('/profile', authMiddleware, UserController.updateProfile);
 
 export default router;
